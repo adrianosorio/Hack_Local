6e747af3e962cfd93801466753272d1d765765a5
OITAO <adrianosorio101@gmail.com>
2015-10-24 16:02:26 -0700

    Adding in a search feature and a real map

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+var dojoConfig = { parseOnLoad: true };
+
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-    center: [-56.049, 38.485],
-    zoom: 3,
-    basemap: "streets"
+    webMapId = "c592d579d0e548b29a743bee10478603";
+    
+require(["esri/map", "esri/arcgis/utils","esri/dijit/Search","esri/geometry/Extent","esri/graphic","esri/symbols/SimpleMarkerSymbol","esri/geometry/screenUtils","dojo/dom", "dojo/dom-construct","dojo/query", "dojo/_base/Color","dojo/domReady!"], function(Map, arcgisUtils, Search, Extent, Graphic, SimpleMarkerSymbol, screenUtils, dom, domConstruct, query, Color) {
+  arcgisUtils.createMap(webMapId, "mapDiv").then(function (response) {
+        map = response.map;
+   
+  //Search application function from arcgis 
+  var search = new Search({
+      map: map,
+    }, dom.byId("search"));
+
+    search.startup();
+  
   });
 });
---
+++ home.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".search_engine").hide();
+  $("#searchButton").on("click", function() {
+  	$(".search_engine").show();
+  });
+});
